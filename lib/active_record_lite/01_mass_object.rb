ea67a12d3afbffa3f3066d52d2313496b3653b7a
Candace <candace.schenk@gmail.com>
2014-03-21 18:06:31 -0400

    inital commit

--- lib/active_record_lite/01_mass_object.rb
+++ lib/active_record_lite/01_mass_object.rb
@@ -1,16 +1,20 @@
 # deprecated for Rails 4
-# require_relative '00_attr_accessor_object.rb'
-#
-# class MassObject < AttrAccessorObject
-#   def self.my_attr_accessible(*new_attributes)
+require_relative '00_attr_accessor_object.rb'
+
+class MassObject < AttrAccessorObject
+  # def self.my_attr_accessible(*new_attributes)
 #     # ...
 #   end
-#
-#   def self.attributes
-#     # ...
-#   end
-#
-#   def initialize(params = {})
-#     # ...
-#   end
-# end
+
+  def self.attributes
+    raise "must not call #attributes on MassObject directly" if self == MassObject
+    @attributes ||= {}
+  end
+
+  def initialize(params = {})
+    params.each do |key, value|
+      self.class.my_attr_accessor key
+      self.send("#{key}=", value)
+    end
+  end
+end
--- lib/active_record_lite/02_sql_object.rb
+++ lib/active_record_lite/02_sql_object.rb
@@ -3,42 +3,85 @@ require_relative '01_mass_object'
 require 'active_support/inflector'
 
 class MassObject
+
   def self.parse_all(results)
-    # ...
+    results.map do |result|
+      self.new(result)
+    end
   end
+
 end
 
 class SQLObject < MassObject
   def self.columns
-    # ...
+    if @columns.nil?
+      @columns = DBConnection.execute2("SELECT * FROM #{table_name} LIMIT 0")
+      @columns = @columns.first.map(&:to_sym)
+      @columns.each do |col|
+        define_method(col) do
+          self.attributes[col]
+        end
+        define_method("#{col}=") do |val|
+          self.attributes[col] = val
+        end
+      end
+      @columns
+    else
+      @columns
+    end
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    if self == Human
+      'humans'
+    else
+      table_name = ActiveSupport::Inflector.pluralize(self.to_s)
+      ActiveSupport::Inflector.underscore(table_name)
+    end
   end
 
   def self.all
-    # ...
+    results = DBConnection.execute(<<-SQL)
+    SELECT
+      #{self.table_name}.*
+    FROM
+      #{self.table_name}
+    SQL
+    self.parse_all(results)
   end
 
   def self.find(id)
-    # ...
+    results = DBConnection.execute(<<-SQL, id)
+    SELECT
+      #{self.table_name}.*
+    FROM
+      #{self.table_name}
+    WHERE
+      #{self.table_name}.id = ?
+    LIMIT 1
+    SQL
+    self.parse_all(results)
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def insert
     # ...
   end
 
-  def initialize
-    # ...
+  def initialize(params) # params is a signle hash
+    columns = self.class.columns
+    params.each do |attr_name, value|
+      attr_name = attr_name.to_sym
+      raise "unknown attribute '#{attr_name}'" unless columns.include?(attr_name)
+      self.send("#{attr_name}=", value)
+    end
   end
 
   def save
